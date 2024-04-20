b5201f622eeaf7070db09df7be0b9d928763ecff
Jinmin Hu <partner.jinmin.hu@philips.com>
2024-04-20 20:43:49 +0200

    add more links

--- hello.py
+++ hello.py
@@ -1,13 +1,19 @@
 from flask import Flask
+from flask import send_from_directory
 
 app = Flask(__name__)
 
-@app.route("/jin")
-def hello_jin():
-    return "<h1> Hello Jin!</h1>"
 @app.route("/")
 def hello_world():
-    return """<table><caption>Phone numbers</caption>
+    return """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>ToDo Application</title>
+</head>
+<body>
+    <table><caption>Phone numbers</caption>
  <thead>
    <tr>
 	<th>Name</th>
@@ -33,4 +39,47 @@ def hello_world():
 	<td>Office</td>
    </tr>
  </tfoot>
-</table>"""
+</table>
+
+</body>
+</html>
+"""
+
+@app.route('/images/<path:path>')
+def send_images(path):
+    return send_from_directory('images', path)
+
+@app.route('/hello/<path:path>')
+def send_hello(path):
+    return f"""
+        Hello {path}
+    """
+@app.route("/jolee")
+def hello_jolee():
+    return """
+<a href="/jin">
+      <img src="/images/cry.jpg" alt="crybaby" width="100" height="48" /><br/>
+      Cry Babies
+</a>
+"""
+
+@app.route("/yinoch")
+def hello_yinoch():
+    return """
+<a href="/eden">
+      <img src="/images/ok.png"/>
+</a>
+"""
+
+@app.route("/eden")
+def hello_eden():
+    return """
+<a href="/iszu">
+      Naar iszu
+</a>
+"""
+@app.route("/jin")
+def hello_jin():
+    return "<h1> Hello Jin!</h1>"
+
+
